f5a70f1e968f18dadc9788987582802e360ff577
Bhavika1012 <bhavikagajipra@gmail.com>
2023-03-16 20:05:46 +0000

    java-week-1-writing-hw-bhavikag

---
+++ src/Programme_8_DivisionOfTwoNumbers.java
@@ -0,0 +1,14 @@
+/**
+ * 8. Write a Java program to divide two numbers and print them on the screen.
+ * Test Data: 50/3
+ * Expected Output: 16
+ */
+public class Programme_8_DivisionOfTwoNumbers {
+    public static void main(String[] args) {
+        int value1 = 50;
+        int value2= 3;
+        int division = value1 / value2;
+        System.out.println( value1 + " divided by " + value2+ " is " + division );
+
+    }
+}
